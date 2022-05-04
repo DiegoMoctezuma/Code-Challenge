d0d106f6d9ebc766cc7151891d08cffa9e8015ee
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-04 13:49:13 -0500

    ACPruebas de la clase Reader

---
+++ test/utils/reader.test.js
@@ -0,0 +1,17 @@
+const Reader = require("../../lib/utils/reader");
+
+describe("Pruebas de Reader", () => {
+	test("Json File", () => {
+		const JsonFile = Reader.readJsonFile("./test/utils/test.json");
+
+		expect(JsonFile[0]).toEqual({
+			id: "6264d5d89f1df827eb84bb23",
+			name: "Warren",
+			email: "Todd@visualpartnership.xyz",
+			credits: 508,
+			enrollments: ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
+			previousCourses: 1,
+			haveCertification: true,
+		});
+	});
+});
