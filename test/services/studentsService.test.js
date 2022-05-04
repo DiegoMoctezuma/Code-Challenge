156ee553470d962681869bb6d27bce8d69e3b0ec
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-04 16:22:14 -0500

    Arreglo de formato con ESLint

--- test/services/studentsService.test.js
+++ test/services/studentsService.test.js
@@ -2,16 +2,16 @@ const Reader = require("../../lib/utils/reader");
 const EstudentsService = require("../../lib/services/studentsService");
 
 describe("Pruebas de EstudentsService", () => {
-	test("1. Filtro de estudiantes por certificación", () => {
-		const students = Reader.readJsonFile("./test/services/test.json");
-		const haveCertification = EstudentsService.getHaveCertification(students);
+    test("1. Filtro de estudiantes por certificación", () => {
+        const students = Reader.readJsonFile("./test/services/test.json");
+        const haveCertification = EstudentsService.getHaveCertification(students);
 
-		expect(haveCertification).toContain("Todd@visualpartnership.xyz");
-	});
-	test("2. Filtro de estudiantes por creditos", () => {
-		const students = Reader.readJsonFile("./test/services/test.json");
-		const haveCredits = EstudentsService.getCredits(students);
+        expect(haveCertification).toContain("Todd@visualpartnership.xyz");
+    });
+    test("2. Filtro de estudiantes por creditos", () => {
+        const students = Reader.readJsonFile("./test/services/test.json");
+        const haveCredits = EstudentsService.getCredits(students);
 
-		expect(haveCredits[0].credits).toBeGreaterThanOrEqual(500);
-	});
+        expect(haveCredits[0].credits).toBeGreaterThanOrEqual(500);
+    });
 });
